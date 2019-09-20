2931af6189523f1f668dd576fce635a83e9fed15
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-20 10:31:44 -0500

    got a working login and colors showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link  } from "react-router-dom";
-
+import PrivateRoute from './components/PrivateRoute'
 import Login from "./components/Login";
 import "./styles.scss";
 import BubblePage from './components/BubblePage';
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Login} />
         <Switch>
-          <PrivateRoute exact path="/protected" component={BubblePage} />
+          <PrivateRoute exact path="/" component={BubblePage} />
           <Route path="/login" component={Login} />
           <Route component={Login} />
         </Switch>
