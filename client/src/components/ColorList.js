9fb78be6dd284c253a33cf47c07000614cb1b9ab
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-20 10:51:13 -0500

    building out editing

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { axiosWithAuth } from "./axiosWithAuth";
 const initialColor = {
   color: "",
   code: { hex: "" }
@@ -10,7 +10,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-
+console.log(colorToEdit)
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -18,10 +18,15 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+     axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors(res.data);
+        setColorToEdit(initialColor);
+      })
+      .catch(err => console.log(err.response));
   };
+  
 
   const deleteColor = color => {
     // make a delete request to delete this color
